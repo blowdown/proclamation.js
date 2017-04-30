4137f3d141ff73bf7606adaa36b9460bf20b063e
blowdown <serg.korotkov.91@gmail.com>
2017-04-30 18:15:10 +0300

    add animation

--- src/proclamation.js
+++ src/proclamation.js
@@ -9,10 +9,20 @@
     function proclamation(text, time) {
         var notification = document.createElement('div');
         container.appendChild(notification);
+
         notification.textContent = text;
         notification.className = 'proclamation';
-        setTimeout(function(){
-            container.removeChild(notification);
+
+        notification.style.height = 0;
+        notification.style.height = notification.clientHeight + 'px';
+
+        setTimeout(function() {
+            notification.style.height = 0;
+            notification.addEventListener('transitionend', function(event) {
+                if (event.propertyName === 'height') {
+                    container.removeChild(notification);
+                }
+            });
         }, time || 5000);
     }
 })(this);
