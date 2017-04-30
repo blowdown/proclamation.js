5cf0d155cfb7daa220b09846301c1943573e14d3
blowdown <serg.korotkov.91@gmail.com>
2017-04-30 19:29:02 +0300

    add normal animations

--- src/proclamation.js
+++ src/proclamation.js
@@ -7,22 +7,57 @@
     container.className = 'proclamation-container';
 
     function proclamation(text, time) {
+        var wrapper = document.createElement('div');
         var notification = document.createElement('div');
-        container.appendChild(notification);
+        wrapper.appendChild(notification);
+        container.appendChild(wrapper);
+
+        wrapper.className = 'proclamation-wrapper';
 
         notification.textContent = text;
         notification.className = 'proclamation';
 
+        var height = notification.clientHeight;
         notification.style.height = 0;
-        notification.style.height = notification.clientHeight + 'px';
 
         setTimeout(function() {
+            notification.style.height = height + 'px';
+        }, 0);
+
+        setTimeout(function() {
+            wrapper.style.paddingTop = 0;
+            wrapper.style.paddingBottom = 0;
             notification.style.height = 0;
+
+            var heightDone = false,
+                paddingTopDone = false,
+                paddingBottomDone = false;
+
             notification.addEventListener('transitionend', function(event) {
+                event.stopPropagation();
                 if (event.propertyName === 'height') {
-                    container.removeChild(notification);
+                    heightDone = true;
+                    remove();
                 }
             });
+
+            wrapper.addEventListener('transitionend', function(event) {
+                event.stopPropagation();
+                if (event.propertyName === 'padding-top') {
+                    paddingTopDone = true;
+                    remove();
+                }
+                if (event.propertyName === 'padding-bottom') {
+                    paddingBottomDone = true;
+                    remove();
+                }
+            });
+
+            function remove() {
+                if (heightDone && paddingTopDone && paddingBottomDone) {
+                    container.removeChild(wrapper);
+                }
+            }
         }, time || 5000);
     }
 })(this);
