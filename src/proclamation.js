14c4f1330a527439c09b5e722195e522cf475b7b
blowdown <serg.korotkov.91@gmail.com>
2017-04-30 17:56:16 +0300

    add timeout

--- src/proclamation.js
+++ src/proclamation.js
@@ -6,10 +6,13 @@
     document.body.appendChild(container);
     container.className = 'proclamation-container';
 
-    function proclamation(text) {
+    function proclamation(text, time) {
         var notification = document.createElement('div');
         container.appendChild(notification);
         notification.textContent = text;
         notification.className = 'proclamation';
+        setTimeout(function(){
+            container.removeChild(notification);
+        }, time || 5000);
     }
 })(this);
